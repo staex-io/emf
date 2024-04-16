c3b28691535d1ad0f83f3bd20c0bebedd3a6e483
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-04-16 23:06:45 +0400

    Implemented a method to create entity

--- emf_contract/lib.rs
+++ emf_contract/lib.rs
@@ -2,136 +2,141 @@
 
 #[ink::contract]
 mod emf_contract {
+    use ink::storage::{traits::StorageLayout, Mapping};
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
     pub struct EmfContract {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        entities: Mapping<AccountId, Entity>,
     }
+
+    #[ink::scale_derive(Encode, Decode, TypeInfo)]
+    #[cfg_attr(feature = "std", derive(StorageLayout))]
+    pub struct Entity {}
 
     impl EmfContract {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self {
+                entities: Mapping::new(),
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default())
+            Self::new()
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn create_entity(&mut self) -> Result<(), String> {
+            if self.entities.get(self.env().caller()).is_some() {
+                return Err("entity is already exists".to_string());
+            }
+            self.entities.insert(self.env().caller(), &Entity {});
+            Ok(())
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
+        pub fn create_sub_entity(&mut self) {}
+
+        #[ink(message)]
+        pub fn delete_sub_entity(&mut self) {}
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
+        use ink::primitives::AccountId;
+
         use super::*;
 
         /// We test if the default constructor does its job.
         #[ink::test]
         fn default_works() {
-            let emf_contract = EmfContract::default();
-            assert!(!emf_contract.get());
+            let _emf_contract = EmfContract::default();
         }
 
         /// We test a simple use case of our contract.
         #[ink::test]
         fn it_works() {
-            let mut emf_contract = EmfContract::new(false);
-            assert!(!emf_contract.get());
-            emf_contract.flip();
-            assert!(emf_contract.get());
+            let mut emf_contract = EmfContract::new();
+            set_sender(default_accounts().alice);
+            emf_contract.create_entity().unwrap();
+            assert!(emf_contract.entities.get(default_accounts().alice).is_some());
+        }
+
+        fn default_accounts() -> ink::env::test::DefaultAccounts<ink::env::DefaultEnvironment> {
+            ink::env::test::default_accounts::<Environment>()
+        }
+
+        fn set_sender(sender: AccountId) {
+            ink::env::test::set_caller::<ink::env::DefaultEnvironment>(sender);
         }
     }
 
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+    // /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
+    // ///
+    // /// When running these you need to make sure that you:
+    // /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
+    // /// - Are running a Substrate node which contains `pallet-contracts` in the background
+    // #[cfg(all(test, feature = "e2e-tests"))]
+    // mod e2e_tests {
+    //     /// Imports all the definitions from the outer scope so we can use them here.
+    //     use super::*;
 
-        /// A helper function used for calling contract messages.
-        use ink_e2e::ContractsBackend;
+    //     /// A helper function used for calling contract messages.
+    //     use ink_e2e::ContractsBackend;
 
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+    //     /// The End-to-End test `Result` type.
+    //     type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let mut constructor = EmfContractRef::default();
+    //     /// We test that we can upload and instantiate the contract using its default constructor.
+    //     #[ink_e2e::test]
+    //     async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+    //         // Given
+    //         let mut constructor = EmfContractRef::default();
 
-            // When
-            let contract = client
-                .instantiate("emf_contract", &ink_e2e::alice(), &mut constructor)
-                .submit()
-                .await
-                .expect("instantiate failed");
-            let call_builder = contract.call_builder::<EmfContract>();
+    //         // When
+    //         let contract = client
+    //             .instantiate("emf_contract", &ink_e2e::alice(), &mut constructor)
+    //             .submit()
+    //             .await
+    //             .expect("instantiate failed");
+    //         let call_builder = contract.call_builder::<EmfContract>();
 
-            // Then
-            let get = call_builder.get();
-            let get_result = client.call(&ink_e2e::alice(), &get).dry_run().await?;
-            assert!(!get_result.return_value());
+    //         // Then
+    //         let get = call_builder.get();
+    //         let get_result = client.call(&ink_e2e::alice(), &get).dry_run().await?;
+    //         assert!(!get_result.return_value());
 
-            Ok(())
-        }
+    //         Ok(())
+    //     }
 
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let mut constructor = EmfContractRef::new(false);
-            let contract = client
-                .instantiate("emf_contract", &ink_e2e::bob(), &mut constructor)
-                .submit()
-                .await
-                .expect("instantiate failed");
-            let mut call_builder = contract.call_builder::<EmfContract>();
+    //     /// We test that we can read and write a value from the on-chain contract contract.
+    //     #[ink_e2e::test]
+    //     async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+    //         // Given
+    //         let mut constructor = EmfContractRef::new(false);
+    //         let contract = client
+    //             .instantiate("emf_contract", &ink_e2e::bob(), &mut constructor)
+    //             .submit()
+    //             .await
+    //             .expect("instantiate failed");
+    //         let mut call_builder = contract.call_builder::<EmfContract>();
 
-            let get = call_builder.get();
-            let get_result = client.call(&ink_e2e::bob(), &get).dry_run().await?;
-            assert!(!get_result.return_value());
+    //         let get = call_builder.get();
+    //         let get_result = client.call(&ink_e2e::bob(), &get).dry_run().await?;
+    //         assert!(!get_result.return_value());
 
-            // When
-            let flip = call_builder.flip();
-            let _flip_result =
-                client.call(&ink_e2e::bob(), &flip).submit().await.expect("flip failed");
+    //         // When
+    //         let flip = call_builder.flip();
+    //         let _flip_result =
+    //             client.call(&ink_e2e::bob(), &flip).submit().await.expect("flip failed");
 
-            // Then
-            let get = call_builder.get();
-            let get_result = client.call(&ink_e2e::bob(), &get).dry_run().await?;
-            assert!(get_result.return_value());
+    //         // Then
+    //         let get = call_builder.get();
+    //         let get_result = client.call(&ink_e2e::bob(), &get).dry_run().await?;
+    //         assert!(get_result.return_value());
 
-            Ok(())
-        }
-    }
+    //         Ok(())
+    //     }
+    // }
 }
