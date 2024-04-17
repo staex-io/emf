c0be9191f266e57ed53827ca8594122f5d81eb60
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-04-17 11:52:45 +0400

    Implement measurement storage and spikes too

--- emf_contract/lib.rs
+++ emf_contract/lib.rs
@@ -6,6 +6,7 @@ mod emf_contract {
     use ink::storage::{traits::StorageLayout, Mapping};
 
     const AVG_DAYS_IN_MONTH: usize = 30;
+    const SECS_IN_23H: u64 = 82_800;
 
     #[ink(storage)]
     pub struct EmfContract {
@@ -28,15 +29,9 @@ mod emf_contract {
     pub struct SubEntity {
         pub entity: AccountId,
         pub location: String,
-        pub measurements: VecDeque<Measurement>,
+        pub measurements: Measurements,
+        pub spikes: Measurements,
         pub deleted: bool,
-    }
-
-    #[ink::scale_derive(Encode, Decode, TypeInfo)]
-    #[cfg_attr(feature = "std", derive(StorageLayout))]
-    pub struct Measurement {
-        pub value: u128,
-        pub timestamp: u64,
     }
 
     #[ink(event)]
@@ -73,6 +68,7 @@ mod emf_contract {
         SubEntityBelongingFailed,
         SubEntityAlreadyDeleted,
         StorageExceeded,
+        MeasurementTooFast,
         Unknown,
     }
 
@@ -82,6 +78,47 @@ mod emf_contract {
                 ink_env::Error::BufferTooSmall => Self::StorageExceeded,
                 _ => Self::Unknown,
             }
+        }
+    }
+
+    #[ink::scale_derive(Encode, Decode, TypeInfo)]
+    #[cfg_attr(feature = "std", derive(StorageLayout))]
+    pub struct Measurement {
+        pub value: u128,
+        pub timestamp: u64,
+    }
+
+    #[ink::scale_derive(Encode, Decode, TypeInfo)]
+    #[cfg_attr(feature = "std", derive(StorageLayout))]
+    pub struct Measurements(VecDeque<Measurement>);
+
+    impl Default for Measurements {
+        fn default() -> Self {
+            Self(VecDeque::with_capacity(AVG_DAYS_IN_MONTH))
+        }
+    }
+
+    impl Measurements {
+        pub fn add(&mut self, value: u128, timestamp: u64) -> Result<bool, EmfError> {
+            // If there are values in vector.
+            // We need to check that previous value was not wrote
+            // in less than 23h.
+            // We check for 23h and not for 24h because we use not real timestamp
+            // but block timestamp which is volatile from time to time.
+            if !self.0.is_empty() {
+                // Unwrap is ok because we checked length.
+                let diff = self.0.back().unwrap().timestamp.checked_sub(timestamp);
+                if diff.is_some() && diff.unwrap() < SECS_IN_23H {
+                    return Err(EmfError::MeasurementTooFast);
+                }
+            }
+            let mut cap_exceeded = false;
+            if self.0.len() == AVG_DAYS_IN_MONTH {
+                cap_exceeded = true;
+                self.0.pop_front();
+            }
+            self.0.push_back(Measurement { value, timestamp });
+            Ok(cap_exceeded)
         }
     }
 
@@ -121,7 +158,8 @@ mod emf_contract {
                 &SubEntity {
                     entity: self.env().caller(),
                     location: location.clone(),
-                    measurements: VecDeque::with_capacity(AVG_DAYS_IN_MONTH),
+                    measurements: Measurements::default(),
+                    spikes: Measurements::default(),
                     deleted: false,
                 },
             )?;
@@ -146,6 +184,7 @@ mod emf_contract {
                     entity: sub_entity_record.entity,
                     location: sub_entity_record.location,
                     measurements: sub_entity_record.measurements,
+                    spikes: sub_entity_record.spikes,
                     deleted: true,
                 },
             )?;
@@ -159,13 +198,37 @@ mod emf_contract {
         #[ink(message)]
         pub fn store_measurement(&mut self, value: u128) -> Result<(), EmfError> {
             let sub_entity_record = self.load_sub_entity(self.env().caller())?;
-            todo!()
+            let mut measurements = sub_entity_record.measurements;
+            measurements.add(value, self.env().block_timestamp())?;
+            self.sub_entities.try_insert(
+                self.env().caller(),
+                &SubEntity {
+                    entity: sub_entity_record.entity,
+                    location: sub_entity_record.location,
+                    measurements,
+                    spikes: sub_entity_record.spikes,
+                    deleted: sub_entity_record.deleted,
+                },
+            )?;
+            Ok(())
         }
 
         #[ink(message)]
-        pub fn store_measurement_spike(&mut self) -> Result<(), EmfError> {
+        pub fn store_measurement_spike(&mut self, value: u128) -> Result<(), EmfError> {
             let sub_entity_record = self.load_sub_entity(self.env().caller())?;
-            todo!()
+            let mut spikes = sub_entity_record.spikes;
+            spikes.add(value, self.env().block_timestamp())?;
+            self.sub_entities.try_insert(
+                self.env().caller(),
+                &SubEntity {
+                    entity: sub_entity_record.entity,
+                    location: sub_entity_record.location,
+                    measurements: sub_entity_record.measurements,
+                    spikes,
+                    deleted: sub_entity_record.deleted,
+                },
+            )?;
+            Ok(())
         }
 
         #[ink(message)]
@@ -315,6 +378,80 @@ mod emf_contract {
             assert_sub_entity_deleted_event(&emitted_events[3], alice, bob);
         }
 
+        /// We test sub-entity measurements storage.
+        #[ink::test]
+        fn sub_entity_store_measurements() {
+            generic_measurements_test(store_measurement, get_measurement)
+        }
+
+        /// We test sub-entity measurement spikes storage.
+        #[ink::test]
+        fn sub_entity_store_measurement_spikes() {
+            generic_measurements_test(store_measurement_spike, get_spike)
+        }
+
+        fn generic_measurements_test<WriteFn, ReadFn>(write_fn: WriteFn, read_fn: ReadFn)
+        where
+            WriteFn: Fn(&mut EmfContract, u128) -> Result<(), EmfError>,
+            ReadFn: Fn(&EmfContract, AccountId, usize) -> Measurement,
+        {
+            let mut emf_contract = EmfContract::new();
+
+            let alice = default_accounts().alice;
+            let bob = default_accounts().bob;
+
+            set_sender(alice);
+            emf_contract.create_entity().unwrap();
+
+            // Check that we cannot store measurement without created sub-entity.
+            set_sender(bob);
+            let err = write_fn(&mut emf_contract, 1).unwrap_err();
+            assert_eq!(EmfError::SubEntityNotFound, err);
+
+            set_sender(alice);
+            emf_contract.create_sub_entity(bob, LOCATION.into()).unwrap();
+
+            set_sender(bob);
+
+            let mut timestamp = 1;
+
+            set_timestamp(timestamp);
+            write_fn(&mut emf_contract, 1).unwrap();
+            assert_eq!(1, read_fn(&emf_contract, bob, 0).value);
+
+            // We need to test that we store exactly 30 values and no more.
+            for i in 2..32 {
+                timestamp += SECS_IN_23H;
+                set_timestamp(timestamp);
+                write_fn(&mut emf_contract, i).unwrap();
+            }
+            assert_eq!(2, read_fn(&emf_contract, bob, 0).value);
+            assert_eq!(31, read_fn(&emf_contract, bob, 29).value);
+            assert_eq!(timestamp, read_fn(&emf_contract, bob, 29).timestamp);
+
+            // Check write measurements too fast.
+            let err = write_fn(&mut emf_contract, 99).unwrap_err();
+            assert_eq!(EmfError::MeasurementTooFast, err);
+
+            // Check that we can't write measurements to delete sub-entity.
+            set_sender(alice);
+            emf_contract.delete_sub_entity(bob).unwrap();
+            set_sender(bob);
+            let err = write_fn(&mut emf_contract, 111).unwrap_err();
+            assert_eq!(EmfError::SubEntityAlreadyDeleted, err);
+        }
+
+        fn store_measurement(emf_contract: &mut EmfContract, value: u128) -> Result<(), EmfError> {
+            emf_contract.store_measurement(value)
+        }
+
+        fn store_measurement_spike(
+            emf_contract: &mut EmfContract,
+            value: u128,
+        ) -> Result<(), EmfError> {
+            emf_contract.store_measurement_spike(value)
+        }
+
         fn assert_entity_created_event(event: &EmittedEvent, entity: AccountId) {
             let evt = decode_event::<EntityCreated>(event);
             assert_eq!(entity, evt.entity);
@@ -342,6 +479,28 @@ mod emf_contract {
             assert_eq!(sub_entity, evt.sub_entity);
         }
 
+        fn get_measurement(
+            emf_contract: &EmfContract,
+            address: AccountId,
+            index: usize,
+        ) -> Measurement {
+            let sub_entity = emf_contract.sub_entities.get(address).unwrap();
+            let inner = sub_entity.measurements.0.get(index).unwrap();
+            Measurement {
+                value: inner.value,
+                timestamp: inner.timestamp,
+            }
+        }
+
+        fn get_spike(emf_contract: &EmfContract, address: AccountId, index: usize) -> Measurement {
+            let sub_entity = emf_contract.sub_entities.get(address).unwrap();
+            let inner = sub_entity.spikes.0.get(index).unwrap();
+            Measurement {
+                value: inner.value,
+                timestamp: inner.timestamp,
+            }
+        }
+
         fn decode_event<T>(event: &EmittedEvent) -> T
         where
             T: ink::scale::Decode,
@@ -355,6 +514,10 @@ mod emf_contract {
 
         fn set_sender(sender: AccountId) {
             ink::env::test::set_caller::<ink::env::DefaultEnvironment>(sender);
+        }
+
+        fn set_timestamp(timestamp: u64) {
+            ink::env::test::set_block_timestamp::<ink::env::DefaultEnvironment>(timestamp);
         }
     }
 }
