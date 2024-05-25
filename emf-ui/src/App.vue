0f7f1d8f21cecf7d18857cc6e030fa576e656f52
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-05-25 09:06:00 +0400

    Initialized UI

---
+++ emf-ui/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+<script>
+import router from '@/router'
+export default {
+  beforeMount() {
+    router.push({ path: window.location.pathname })
+  },
+}
+</script>
+
+<template>
+  <header>
+    <nav>
+      <a href="/devices">
+        <img class="logo" alt="Staex logo" src="@/assets/logo-light.svg" />
+      </a>
+    </nav>
+  </header>
+  <RouterView />
+</template>
