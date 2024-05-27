9e682a56313b650b7054e63c0de63bc6b9ae1b1f
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-05-27 11:25:24 +0400

    Add Germany map with bounds

--- emf-ui/src/views/MapView.vue
+++ emf-ui/src/views/MapView.vue
@@ -1,7 +1,32 @@
 <script>
-export default {}
+import * as L from 'leaflet'
+
+export default {
+  mounted() {
+    const southWest = L.latLng(55.229, 3.779),
+      northEast = L.latLng(47.339, 15.667),
+      bounds = L.latLngBounds(southWest, northEast)
+    const map = L.map('map', {
+      attributionControl: false,
+      maxBounds: bounds,
+    }).setView([52.523, 13.381], 10)
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      minZoom: 8,
+      maxZoom: 12,
+    }).addTo(map)
+  },
+}
 </script>
 
-<template>map</template>
+<template>
+  <div id="map" />
+</template>
 
-<style scoped></style>
+<style scoped>
+#map {
+  height: 500px;
+  width: 100%;
+  margin-top: 1px;
+  border-radius: 10px;
+}
+</style>
