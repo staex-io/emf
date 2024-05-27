a03f219ba575792fbe38d203c5ff4794b9ddd715
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-05-27 15:05:25 +0400

    Improve popup with card and information about cell tower

--- emf-ui/src/views/MapView.vue
+++ emf-ui/src/views/MapView.vue
@@ -2,6 +2,11 @@
 import * as L from 'leaflet'
 
 export default {
+  data() {
+    return {
+      id: '[VantageTowers] Tower#1',
+    }
+  },
   mounted() {
     const southWest = L.latLng(55.229, 3.779),
       northEast = L.latLng(47.339, 15.667),
@@ -9,6 +14,7 @@ export default {
     const map = L.map('map', {
       attributionControl: false,
       maxBounds: bounds,
+      doubleClickZoom: false,
     }).setView([52.523, 13.381], 10)
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       minZoom: 8,
@@ -17,9 +23,29 @@ export default {
 
     const towerIcon = L.icon({
       iconUrl: 'tower.svg',
-      iconSize: [30, 30],
+      iconSize: [50, 50],
     })
-    L.marker([52.523, 13.381], { icon: towerIcon }).addTo(map).bindPopup('VantageTowers Tower#1')
+    L.marker([52.523, 13.381], {
+      icon: towerIcon,
+      riseOnHover: true,
+      riseOffset: 250,
+    })
+      .addTo(map)
+      .on('click', () => {
+        L.popup([52.523, 13.381], {
+          content: `
+  <div class="card-static">
+    <div class="card-header">${this.id}</div>
+    <div class="card-content">
+      <div class="card-field">
+        <span class="card-field-label">ID</span>
+        <span class="card-field-value">${this.id}</span>
+      </div>
+    </div>
+  </div>
+            `,
+        }).openOn(map)
+      })
   },
 }
 </script>
